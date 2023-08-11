e3c59a99e80aab087af1889757b79b8d342caa45
krishpat0 <krishpat001800@gmail.com>
2023-08-11 14:04:45 +0530

    JAVA Day1

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Krishna");
+        int age;
+         age=18 ; 
+        System.out.println(age);
+
+    }
+}
